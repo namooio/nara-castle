c2209eaead61484ad1d71c70aa81cdbbe0119dda
kchuh <kchuh@nextree.co.kr>
2016-04-23 11:53:13 +0900

    Refectoring.

---
+++ nara-castle-client/src/test/java/namoo/nara/castle/client/CastleClientTest.java
@@ -0,0 +1,24 @@
+package namoo.nara.castle.client;
+
+import namoo.nara.castle.adapter.dto.CastleBuildDto;
+import namoo.nara.share.restclient.NaraRestClient;
+import namoo.nara.share.restclient.jaxrs.JaxRSClient;
+
+import java.util.Locale;
+
+/**
+ * Created by kchuh@nextree.co.kr on 2016. 4. 23..
+ */
+public class CastleClientTest {
+    //
+    public void buildCastle() {
+        //
+        NaraRestClient naraRestClient = new JaxRSClient("http://localhost:9930");
+        CastleClient castleClient = new CastleClient(naraRestClient);
+
+        CastleBuildDto castleBuildDto = new CastleBuildDto();
+        castleBuildDto.setName("허기철");
+        castleBuildDto.setLocale(Locale.KOREAN);
+        castleClient.buildCastle("0000", castleBuildDto);
+    }
+}
