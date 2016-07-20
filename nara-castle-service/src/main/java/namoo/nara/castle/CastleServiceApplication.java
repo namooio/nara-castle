b88f487e54263be410c0c11b65642c34a62827fe
kchuh <kchuh@nextree.co.kr>
2016-07-20 14:04:15 +0900

    Remove proxy component.

--- nara-castle-service/src/main/java/namoo/nara/castle/CastleServiceApplication.java
+++ nara-castle-service/src/main/java/namoo/nara/castle/CastleServiceApplication.java
@@ -1,10 +1,7 @@
 package namoo.nara.castle;
 
-import namoo.nara.castle.cp.spring.CastleProxySpringLycler;
-import namoo.nara.castle.domain.proxy.CastleProxyLycler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(
 		scanBasePackages = {
@@ -16,13 +13,6 @@ public class CastleServiceApplication {
 	//
 	public static void main(String[] args) {
 		SpringApplication.run(CastleServiceApplication.class, args);
-	}
-
-
-	@Bean
-	public CastleProxyLycler createCastleProxyLycler() {
-		//
-		return new CastleProxySpringLycler();
 	}
 
 }
