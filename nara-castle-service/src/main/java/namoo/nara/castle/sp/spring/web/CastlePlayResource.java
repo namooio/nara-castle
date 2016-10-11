f51779ac1221d3e6feee29545c4eb072e4d3195b
kchuh <kchuh@nextree.co.kr>
2016-10-11 13:42:01 +0900

    Castle renewal

--- nara-castle-service/src/main/java/namoo/nara/castle/sp/spring/web/CastlePlayResource.java
+++ nara-castle-service/src/main/java/namoo/nara/castle/sp/spring/web/CastlePlayResource.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Controller
-public class PlayFrontView {
+public class CastlePlayResource {
     //
     @RequestMapping(value = "castle-play", method = RequestMethod.GET)
     public ModelAndView play(@RequestParam("pavilionId") String pavilionId, @RequestParam("castingId") String castingId, @RequestParam("playerId") String playerId) {
