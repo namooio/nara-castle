5157793ea4a153c6d8a06a1efeb84d9b3e2f2321
kchuh <kchuh@nextree.co.kr>
2016-10-11 09:47:23 +0900

    Castle renewal

--- nara-castle-boot/src/main/java/namoo/nara/castle/CastleApplication.java
+++ nara-castle-boot/src/main/java/namoo/nara/castle/CastleApplication.java
@@ -3,12 +3,7 @@ package namoo.nara.castle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-		scanBasePackages = {
-				"namoo.nara.castle",
-				"namoo.nara.stage"
-		}
-)
+@SpringBootApplication
 public class CastleApplication {
 	//
 	public static void main(String[] args) {
