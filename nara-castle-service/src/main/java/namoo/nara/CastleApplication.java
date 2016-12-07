e0b94d40f9693dee848e656dd9d560f3c86f806d
kchuh <kchuh@nextree.co.kr>
2016-12-07 16:38:27 +0900

    Remove boot module

--- nara-castle-service/src/main/java/namoo/nara/CastleApplication.java
+++ nara-castle-service/src/main/java/namoo/nara/CastleApplication.java
@@ -1,4 +1,4 @@
-package namoo.nara.castle;
+package namoo.nara;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- nara-castle-service/src/main/java/namoo/nara/castle/cp/spring/CastleServiceSpringLycler.java
+++ nara-castle-service/src/main/java/namoo/nara/castle/cp/spring/CastleServiceSpringLycler.java
@@ -1,20 +1,24 @@
 package namoo.nara.castle.cp.spring;
 
+import namoo.nara.castle.domain.logic.CastleServiceLogic;
 import namoo.nara.castle.domain.service.CastleService;
 import namoo.nara.castle.domain.service.CastleServiceLycler;
+import namoo.nara.castle.domain.store.CastleStoreLycler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CastleServiceSpringLycler implements CastleServiceLycler {
     //
     @Autowired
-    private CastleService castleService;
+    private CastleStoreLycler storeLycler;
 
+    @Bean
     @Override
     public CastleService requestCastleService() {
         //
-        return castleService;
+        return new CastleServiceLogic(storeLycler);
     }
 
 }
