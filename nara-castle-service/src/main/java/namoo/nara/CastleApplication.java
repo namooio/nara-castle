fa1566f030ffd6f2824e4e01fb17e7796544c892
kchuh <kchuh@nextree.co.kr>
2017-01-04 18:05:12 +0900

    Add event sourcing

--- nara-castle-service/src/main/java/namoo/nara/CastleApplication.java
+++ nara-castle-service/src/main/java/namoo/nara/CastleApplication.java
@@ -1,7 +1,15 @@
 package namoo.nara;
 
+import namoo.nara.castle.es.handler.internal.CastleBuiltEventHandler;
+import namoo.nara.castle.es.listener.internal.CastleBuiltEventRabbitListener;
+import namoo.nara.castle.event.CastleBuiltEvent;
 import namoo.nara.castle.sa.rbmp.RabbitEventProxy;
 import namoo.nara.share.event.NaraEventProxy;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -18,5 +26,32 @@ public class CastleApplication {
 		//
 		return new RabbitEventProxy();
 	}
+
+	/************************************************************************************/
+	@Bean(name = "castleBuiltEventQueue")
+	public Queue castleBuiltEventQueue() {
+		return new Queue("CASTLE.CASTLE_BUILT_EVENT_QUEUE");
+	}
+
+	@Bean(name = "castleBuiltEventTopicExchange")
+	public TopicExchange castleBuiltEventTopicExchange() {
+		return new TopicExchange(CastleBuiltEvent.TOPIC_EXCHANGE);
+	}
+
+	@Bean
+	public Binding castleBuiltEventBinding(
+			@Qualifier("castleBuiltEventQueue") Queue queue,
+			@Qualifier("castleBuiltEventTopicExchange") TopicExchange exchange
+	) {
+		return BindingBuilder.bind(queue).to(exchange).with("#");
+	}
+
+	@Bean
+	public CastleBuiltEventRabbitListener castleBuiltEventRabbitListener() {
+		CastleBuiltEventRabbitListener listener = new CastleBuiltEventRabbitListener();
+		listener.addHandler(new CastleBuiltEventHandler());
+		return listener;
+	}
+	/************************************************************************************/
 
 }
---
+++ nara-castle-service/src/main/java/namoo/nara/castle/es/handler/internal/CastleBuiltEventHandler.java
@@ -0,0 +1,21 @@
+package namoo.nara.castle.es.handler.internal;
+
+import namoo.nara.castle.domain.context.CastleContext;
+import namoo.nara.castle.domain.service.CastleService;
+import namoo.nara.castle.event.CastleBuiltEvent;
+import namoo.nara.share.event.NaraEventHandler;
+
+public class CastleBuiltEventHandler implements NaraEventHandler<CastleBuiltEvent> {
+    //
+    @Override
+    public void handle(CastleBuiltEvent castleBuiltEvent) {
+        //
+        CastleService castleService = CastleContext.getServiceLycler().requestCastleService();
+
+        String castleId = castleBuiltEvent.getCastleId();
+        String metroId = castleBuiltEvent.getOriginMetroId();
+        String citizenId = castleBuiltEvent.getOriginCitizenId();
+
+        castleService.addJoinedMetro(castleId, metroId, citizenId);
+    }
+}
