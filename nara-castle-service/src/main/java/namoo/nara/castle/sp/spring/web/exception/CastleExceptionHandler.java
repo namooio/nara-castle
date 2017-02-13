73aa76d324c33b80451aa8928633ccfa4cafa4b4
kchuh <kchuh@nextree.co.kr>
2017-02-13 17:56:41 +0900

    Exception log 추가

--- nara-castle-service/src/main/java/namoo/nara/castle/sp/spring/web/exception/CastleExceptionHandler.java
+++ nara-castle-service/src/main/java/namoo/nara/castle/sp/spring/web/exception/CastleExceptionHandler.java
@@ -1,5 +1,7 @@
 package namoo.nara.castle.sp.spring.web.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,10 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CastleExceptionHandler {
     //
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleException(Exception e) {
         //
+        logger.error(e.getMessage(), e);
         return e.getMessage();
     }
 
