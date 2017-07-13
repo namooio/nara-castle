1d549a57e331cfd423d67df26dffcc9e81c8dce4
kchuh <kchuh@nextree.co.kr>
2017-07-13 11:34:33 +0900

    beta1 working. akka prototype

--- nara-castle-service-play/app/namoo/nara/castle/cp/CastleViewMongoStoreLycler.java
+++ nara-castle-service-play/app/namoo/nara/castle/cp/CastleViewMongoStoreLycler.java
@@ -1,8 +1,7 @@
 package namoo.nara.castle.cp;
 
 import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
+import com.mongodb.MongoClientURI;
 import com.typesafe.config.ConfigFactory;
 import namoo.nara.castle.da.mongo.CastellanViewMongoStore;
 import namoo.nara.castle.da.mongo.CastleViewMongoStore;
@@ -13,8 +12,6 @@ import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 
 import javax.inject.Singleton;
-import java.util.ArrayList;
-import java.util.List;
 
 @Singleton
 public class CastleViewMongoStoreLycler implements CastleViewStoreLycler {
@@ -28,20 +25,22 @@ public class CastleViewMongoStoreLycler implements CastleViewStoreLycler {
 
         morphia.mapPackage("namoo.nara.castle.da.mongo.document");
 
-        String host = ConfigFactory.load().getString("mongo.host");
-        int port = ConfigFactory.load().getInt("mongo.port");
+//        String host = ConfigFactory.load().getString("mongo.host");
+//        int port = ConfigFactory.load().getInt("mongo.port");
+//
+//        String database = ConfigFactory.load().getString("mongo.database");
+//        String username = ConfigFactory.load().getString("mongo.username");
+//        String password = ConfigFactory.load().getString("mongo.password");
+//
+//        List<ServerAddress> addresses = new ArrayList<>();
+//        addresses.add(new ServerAddress(host, port));
+//        List<MongoCredential> credentials = new ArrayList<>();
+//        credentials.add(MongoCredential.createCredential(username, database, password.toCharArray()));
 
-        String database = ConfigFactory.load().getString("mongo.database");
-        String username = ConfigFactory.load().getString("mongo.username");
-        String password = ConfigFactory.load().getString("mongo.password");
+        String mongoUri = ConfigFactory.load().getString("mongo.uri");
+        MongoClient mongoClient = new MongoClient(new MongoClientURI(mongoUri));
 
-        List<ServerAddress> addresses = new ArrayList<>();
-        addresses.add(new ServerAddress(host, port));
-        List<MongoCredential> credentials = new ArrayList<>();
-        credentials.add(MongoCredential.createCredential(username, database, password.toCharArray()));
-        MongoClient mongoClient = new MongoClient(addresses, credentials);
-
-        Datastore datastore = morphia.createDatastore(mongoClient, database);
+        Datastore datastore = morphia.createDatastore(mongoClient, "nara_castle");
 
         this.castleViewStore = new CastleViewMongoStore(datastore);
         this.castellanViewStore = new CastellanViewMongoStore(datastore);
