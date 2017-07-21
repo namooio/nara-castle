3f89184a83f28640f2b147c8a7764db83e96221d
kchuh <kchuh@nextree.co.kr>
2017-07-21 15:54:08 +0900

    beta1 working.

--- nara-castle-adapter/src/test/java/nara/castle/adapter/rest/CastleRestAdapterTest.java
+++ nara-castle-adapter/src/test/java/nara/castle/adapter/rest/CastleRestAdapterTest.java
@@ -1,13 +1,7 @@
 package nara.castle.adapter.rest;
 
-import nara.castle.domain.castle.command.AddEnrollmentCommand;
 import nara.castle.domain.castle.command.BuildCastleCommand;
-import nara.castle.domain.castle.command.ModifyCastellanCommand;
-import nara.castle.domain.castle.command.WithdrawMetroCommand;
-import nara.castle.domain.castle.entity.Contact;
 import nara.castle.domain.castle.entity.Enrollment;
-import nara.castle.domain.castlequery.model.CastellanRM;
-import nara.share.domain.NameValueList;
 import nara.share.domain.granule.Email;
 import nara.share.domain.granule.Name;
 import nara.share.domain.granule.NaraZone;
@@ -24,7 +18,7 @@ public class CastleRestAdapterTest {
         NaraRestClient naraRestClient = new SpringWebRestClient("http://localhost:9000");
         CastleRestAdapter castleRestAdapter = new CastleRestAdapter(naraRestClient);
 
-        for (int i = 2 ; i < 3 ; i++) {
+        for (int i = 0 ; i < 100 ; i++) {
             String seq = "0" + i;
 
             String metroId = "P" + seq;
@@ -41,39 +35,39 @@ public class CastleRestAdapterTest {
 
             String castellanId = castleRestAdapter.buildCastle(new BuildCastleCommand(enrollment)).toCompletableFuture().get();
 
-            while(!castleRestAdapter.existsCastellan(castellanId).toCompletableFuture().get()) {
-                //
-                Thread.sleep(1000);
-            }
-
-            CastellanRM castellanRM = castleRestAdapter.findCastellan(castellanId).toCompletableFuture().get();
-            Contact contact = castellanRM.getContact();
-            contact.getEmails().add(new Email("michael7557@naver.com"));
-
-            NameValueList nameValues = new NameValueList();
-            nameValues.add("displayName", "허허허");
-            nameValues.add("primaryEmail", "michael7557@gmail.com");
-            nameValues.add("contact", contact.toJson());
-
-            ModifyCastellanCommand modifyCastellanCommand = new ModifyCastellanCommand(castellanId, nameValues);
-            castleRestAdapter.modifyCastellan(castellanId, modifyCastellanCommand).toCompletableFuture().get();
-
-            enrollment = new Enrollment(metroId2, civilianId2,
-                    new Name(Locale.KOREAN, "유유", "호"),
-                    new Email("kchuh@me.com"),
-                    new NaraZone(Locale.KOREA, "Asia/Seoul")
-            );
-            AddEnrollmentCommand addEnrollmentCommand = new AddEnrollmentCommand(castellanId, enrollment);
-            castleRestAdapter.addEnrollment(castellanId, addEnrollmentCommand).toCompletableFuture().get();
-
-            Boolean enrolled = castleRestAdapter.checkEnrolled(castellanId, metroId).toCompletableFuture().get();
-            System.out.println(enrolled);
-
-            WithdrawMetroCommand withdrawMetroCommand = new WithdrawMetroCommand(castellanId, metroId, civilianId);
-            castleRestAdapter.withdrawMetro(castellanId, withdrawMetroCommand).toCompletableFuture().get();
-
-            enrolled = castleRestAdapter.checkEnrolled(castellanId, metroId).toCompletableFuture().get();
-            System.out.println(enrolled);
+//            while(!castleRestAdapter.existsCastellan(castellanId).toCompletableFuture().get()) {
+//                //
+//                Thread.sleep(1000);
+//            }
+//
+//            CastellanRM castellanRM = castleRestAdapter.findCastellan(castellanId).toCompletableFuture().get();
+//            Contact contact = castellanRM.getContact();
+//            contact.getEmails().add(new Email("michael7557@naver.com"));
+//
+//            NameValueList nameValues = new NameValueList();
+//            nameValues.add("displayName", "허허허");
+//            nameValues.add("primaryEmail", "michael7557@gmail.com");
+//            nameValues.add("contact", contact.toJson());
+//
+//            ModifyCastellanCommand modifyCastellanCommand = new ModifyCastellanCommand(castellanId, nameValues);
+//            castleRestAdapter.modifyCastellan(castellanId, modifyCastellanCommand).toCompletableFuture().get();
+//
+//            enrollment = new Enrollment(metroId2, civilianId2,
+//                    new Name(Locale.KOREAN, "유유", "호"),
+//                    new Email("kchuh@me.com"),
+//                    new NaraZone(Locale.KOREA, "Asia/Seoul")
+//            );
+//            AddEnrollmentCommand addEnrollmentCommand = new AddEnrollmentCommand(castellanId, enrollment);
+//            castleRestAdapter.addEnrollment(castellanId, addEnrollmentCommand).toCompletableFuture().get();
+//
+//            Boolean enrolled = castleRestAdapter.checkEnrolled(castellanId, metroId).toCompletableFuture().get();
+//            System.out.println(enrolled);
+//
+//            WithdrawMetroCommand withdrawMetroCommand = new WithdrawMetroCommand(castellanId, metroId, civilianId);
+//            castleRestAdapter.withdrawMetro(castellanId, withdrawMetroCommand).toCompletableFuture().get();
+//
+//            enrolled = castleRestAdapter.checkEnrolled(castellanId, metroId).toCompletableFuture().get();
+//            System.out.println(enrolled);
         }
 
 
