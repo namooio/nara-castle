6667f621c4d390f43656ad9883c4ab524939420f
kchuh <kchuh@nextree.co.kr>
2017-07-24 10:51:32 +0900

    beta1 working.

--- nara-castle-actor-akka/src/main/java/nara/castle/actor/akka/query/CastleQueryActor.java
+++ nara-castle-actor-akka/src/main/java/nara/castle/actor/akka/query/CastleQueryActor.java
@@ -4,7 +4,10 @@ import akka.actor.Props;
 import nara.castle.domain.castlequery.model.EnrollmentRM;
 import nara.castle.domain.castlequery.model.KeyAttr;
 import nara.castle.domain.castlequery.query.*;
+import nara.castle.domain.castlequery.store.CastellanRMStore;
 import nara.castle.domain.castlequery.store.CastleRMStoreLycler;
+import nara.castle.domain.castlequery.store.EnrollmentRMStore;
+import nara.castle.domain.castlequery.store.UnitPlateRMStore;
 import nara.share.actor.akka.NaraActor;
 import nara.share.domain.protocol.NaraQuery;
 
@@ -12,7 +15,9 @@ import java.util.List;
 
 public class CastleQueryActor extends NaraActor {
     //
-    private CastleRMStoreLycler rmStoreLycler;
+    private CastellanRMStore castellanRMStore;
+    private EnrollmentRMStore enrollmentRMStore;
+    private UnitPlateRMStore unitPlateRMStore;
 
     static public Props props(CastleRMStoreLycler rmStoreLycler) {
         //
@@ -21,7 +26,9 @@ public class CastleQueryActor extends NaraActor {
 
     public CastleQueryActor(CastleRMStoreLycler rmStoreLycler) {
         //
-        this.rmStoreLycler = rmStoreLycler;
+        castellanRMStore = rmStoreLycler.requestCastellanRMStore();
+        enrollmentRMStore = rmStoreLycler.requestEnrollmentRMStore();
+        unitPlateRMStore = rmStoreLycler.requestUnitPlateRMStore();
     }
 
     @Override
@@ -31,28 +38,28 @@ public class CastleQueryActor extends NaraActor {
         .match(FindCastellanQuery.class, findCastellanQuery -> {
             //
             String castellanId = findCastellanQuery.getCastellanId();
-            responseResult(() -> rmStoreLycler.requestCastellanRMStore().retrieve(castellanId));
+            responseResult(() -> castellanRMStore.retrieve(castellanId));
         })
         .match(FindCastellansQuery.class, findCastellansQuery -> {
             //
-            responseResult(() -> rmStoreLycler.requestCastellanRMStore().retrieveAll());
+            responseResult(() -> castellanRMStore.retrieveAll());
         })
         .match(ExistenceCheckQuery.class, existenceCheckQuery -> {
             //
             String castellanId = existenceCheckQuery.getCastellanId();
-            responseResult(() -> rmStoreLycler.requestCastellanRMStore().exists(castellanId));
+            responseResult(() -> castellanRMStore.exists(castellanId));
         })
         .match(FindEnrollmentsQuery.class, findEnrollmentsQuery -> {
             //
             String castellanId = findEnrollmentsQuery.getCastellanId();
-            responseResult(() -> rmStoreLycler.requestEnrollmentRMStore().retrieveByCastellanId(castellanId));
+            responseResult(() -> enrollmentRMStore.retrieveByCastellanId(castellanId));
         })
         .match(EnrolledCheckQuery.class, enrolledCheckQuery -> {
             //
             String castellanId = enrolledCheckQuery.getCastellanId();
             String metroId = enrolledCheckQuery.getMetroId();
             responseResult(() -> {
-                List<EnrollmentRM> enrollmentRMS = rmStoreLycler.requestEnrollmentRMStore().retrieveByCastellanId(castellanId);
+                List<EnrollmentRM> enrollmentRMS = enrollmentRMStore.retrieveByCastellanId(castellanId);
                 EnrollmentRM found = enrollmentRMS.stream().filter(enrollmentRM -> enrollmentRM.getMetroId().equals(metroId)).findFirst().orElse(null);
                 return found != null;
             });
@@ -61,7 +68,7 @@ public class CastleQueryActor extends NaraActor {
             //
             KeyAttr keyAttr = findUnitPlatesQuery.getKeyAttr();
             String keyValue = findUnitPlatesQuery.getKeyValue();
-            responseResult(() -> rmStoreLycler.requestUnitPlateRMStore().retrieve(keyAttr, keyValue));
+            responseResult(() -> unitPlateRMStore.retrieve(keyAttr, keyValue));
         })
         .onMessage(query);
     }
