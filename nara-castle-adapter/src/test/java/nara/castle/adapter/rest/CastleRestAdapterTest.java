316952a9318ce0d001ddbb48155d81d6e79b19cd
kchuh <kchuh@nextree.co.kr>
2017-07-25 12:56:04 +0900

    beta1 working.

--- nara-castle-adapter/src/test/java/nara/castle/adapter/rest/CastleRestAdapterTest.java
+++ nara-castle-adapter/src/test/java/nara/castle/adapter/rest/CastleRestAdapterTest.java
@@ -18,7 +18,7 @@ public class CastleRestAdapterTest {
         NaraRestClient naraRestClient = new SpringWebRestClient("http://localhost:9000");
         CastleRestAdapter castleRestAdapter = new CastleRestAdapter(naraRestClient);
 
-        for (int i = 300 ; i < 302 ; i++) {
+        for (int i = 0 ; i < 10 ; i++) {
             String seq = "0" + i;
 
             String metroId = "P" + seq;
